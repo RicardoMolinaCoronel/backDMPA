62c130d13cd5556565f9377c9d96881fb66ac406
RicardoMolinaCoronel <ridumoli@espol.edu.ec>
2023-09-04 14:36:24 -0500

    Cambios documentacion

--- routes/rest_cliente.js
+++ routes/rest_cliente.js
@@ -8,44 +8,9 @@ const pedido = require('../models').pedido; // Asegúrate de importar el modelo 
 const clientepedido = require('../models').cliente_pedido; // Asegúrate de importar el modelo ClientePedido
 pedido.hasMany(clientepedido);
 
-router.get('/findAllPedidosByClientId/:id/json', function(req, res, next) { 
- 
-    const { rol } = req.user;
-
-    if (rol !== 'admin') {
-        return res.sendStatus(403);
-    }
-	let id = req.params.id;
-pedido.findAll({
-  include: [
-    {
-      model: clientepedido,
-required: true,
-where: {
-      cliente_idcliente: id
-    },
-     
-    },
-  ],
-
-})
-          .then(instancia => {
-            if(instancia) {
-              res.status(200).json(instancia);
-            } else {
-              res.status(404).json({error: "No existe registro con el identificador "+id})
-            }
-          })
-          .catch(error => res.status(400).send(error))
-
-    });
 
 router.get('/findAll/json', function(req, res, next) {  
     const { rol } = req.user;
-
-    if (rol !== 'admin') {
-        return res.sendStatus(403);
-    }
 
     	/* MÉTODO ESTÁTICO findAll  */
 
